e9c7ccfd40c9aedc0401a023c4a609fc79d48663
jackrechard <10936373062qq>
2021-02-02 10:32:34 +0800

    tag1.0.5

--- src/spider/spider_maven/spider.java
+++ src/spider/spider_maven/spider.java
@@ -19,7 +19,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-//gengxintag
+//gengxintag1.0.5
 public class spider {
     static WebDriver driver;
 
